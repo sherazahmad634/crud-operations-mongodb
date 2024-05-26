d0e6fa055f734f313b793aa2bb580b093dddef87
Sheraz Ahmad <sherazahmmad634@gmail.com>
2024-05-26 14:46:29 +0500

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import TopicsList from "@/components/topic-list";
+export default function Home() {
+  return (
+    <>
+      <TopicsList />
+     
+    </>
+  );
+}
